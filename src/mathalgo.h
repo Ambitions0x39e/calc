d44fd019534cb9346000303f0e1a067891552d0f
Ambitions0x39e <gwuchanga@gmail.com>
2023-11-29 09:52:04 +0800

    Update mathalgo.h
    add todo

--- src/mathalgo.h
+++ src/mathalgo.h
@@ -1,12 +1,12 @@
 #include "stdio.h"
 #define _int64 long long
-long long QuickPow(_int64 x, _int64 n)
+_int64 QuickPow(_int64 x, _int64 n)
 {
     if (n == 0)
     {
         return 1;
     }
-    _int64 res=QuickPow(x, n / 2);
+    _int64 res = QuickPow(x, n / 2);
     if (n % 2)
     {
         return res * res * x;
@@ -14,5 +14,20 @@ long long QuickPow(_int64 x, _int64 n)
     else
     {
         return res * res;
+    }
+}
+/*
+TODO: 
+- Permutation support
+    - _int64
+- Combination support
+    - _int64
+    
+*/
+_int64 Permutation(_int64 n, _int64 m)
+{
+    _int64 result=1;
+    for(int i=0;i<m;i++) {
+        
     }
 }
