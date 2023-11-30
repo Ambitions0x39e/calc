b48f3c311b9c0986c070a10a0cf0fdb65d622943
Ambitions0x39e <gwuchanga@gmail.com>
2023-11-30 15:04:58 +0800

    Create install.sh
    starting to work on install.sh, running
    - g++ install
    - compile c files

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/zsh
+xcode-select --install
+cd Probability\ Distribution
+g++ probability_calculator_fraction.cpp -o probability_calculator_fraction -O2
+g++ probability_calculator_float.cpp -o probability_calculator_float -O2
