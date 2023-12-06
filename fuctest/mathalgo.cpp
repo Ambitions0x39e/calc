d1097394bd97c9ee9ad8c7df6c8aa10824f04582
Ambitions0x39e <gwuchanga@gmail.com>
2023-12-06 09:34:06 +0800

    update

---
+++ fuctest/mathalgo.cpp
@@ -0,0 +1,10 @@
+#include "iostream"
+#include "../src/mathalgo.h"
+using namespace std;
+
+int main() 
+{
+    int a,b;
+    cin>>a>>b;
+    cout<<QuickPow(a,b);
+}
--- install.sh
+++ install.sh
@@ -3,3 +3,4 @@ xcode-select --install
 cd Probability\ Distribution
 g++ probability_calculator_fraction.cpp -o probability_calculator_fraction -O2
 g++ probability_calculator_float.cpp -o probability_calculator_float -O2
+echo "C++ files successfully compiled!"
