50480518af81d52183e34b8cd98ee948050e5f06
Ambitions0x39e <gwuchanga@gmail.com>
2023-12-08 23:16:36 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,11 +8,13 @@ if ! command -v clang >/dev/null 2>&1; then
   exit 1
 fi
 echo "Xcode command line tool is successfully installed." >&2
-
+echo "Compiling C++ files..." >&2
 sleep 5
 mkdir exes
 cd Probability\ Distribution
 g++ probability_calculator_fraction.cpp -o ../exes/probability_calculator_fraction -O2 -lm
 g++ probability_calculator_float.cpp -o ../exes/probability_calculator_float -O2 -lm
-chmod +x probability_calculator_fraction probability_calculator_float
+cd ../exes
+chmod +x ./probability_calculator_float ./probability_calculator_fraction
+cd ../
 echo "C++ files successfully compiled!" >&2
