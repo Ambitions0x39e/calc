2f0e105fb29b534dc1ac3df249cac856727c9b12
Ambitions0x39e <gwuchanga@gmail.com>
2023-12-10 10:39:45 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,18 +15,17 @@ mkdir exes
 echo "Compile: Probability Distribution" >&2
 sleep 2
 cd Probability\ Distribution
-g++ probability_calculator_fraction.cpp -o ../exes/probability_calculator_fraction -O2 -lm
-g++ probability_calculator_float.cpp -o ../exes/probability_calculator_float -O2 -lm
-cd ../exes
-chmod +x ./probability_calculator_float ./probability_calculator_fraction
-cd ../
+  g++ probability_calculator_fraction.cpp -o ../exes/probability_calculator_fraction -O2 -lm
+  g++ probability_calculator_float.cpp -o ../exes/probability_calculator_float -O2 -lm
+  cd ../exes
+   chmod +x ./probability_calculator_float ./probability_calculator_fraction
+  cd ../
 # Compile Files about Variance & SD
 echo "Compile: Variance & Standard Deviation" >&2
 sleep 2
 cd Variance\ and\ SD
-g++ variance.cpp -o ../exes/variance -O2 -lm
-cd ../exes
-chmod +x ./variance
-cd ../
-
+  g++ variance.cpp -o ../exes/variance -O2 -lm
+  cd ../exes
+    chmod +x ./variance
+  cd ../
 echo "C++ files successfully compiled!" >&2
