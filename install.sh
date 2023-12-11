c2aa8af29ec7a9f665b07c82c0db29a7c403b9cb
Ambitions0x39e <gwuchanga@gmail.com>
2023-12-11 12:56:11 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #!/bin/zsh
-rm -rf exes
-echo "Checking Xcode command line tools..." >&2
+echo "Checking Environment Dependencies" >&2
 sleep 5
 if ! command -v clang >/dev/null 2>&1; then
   echo "Xcode command line tools not installed. Installing..." >&2
