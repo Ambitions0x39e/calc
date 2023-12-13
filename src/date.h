42bbb580edd99d10fd7a3e264e165b309e5f28ef
Ambitions0x39e <gwuchanga@gmail.com>
2023-12-13 08:08:36 +0800

    Update date.h

--- src/date.h
+++ src/date.h
@@ -10,16 +10,9 @@ TODO:
 class date
 {
 public:
-    void __re_alloc()
+    void __update()
     {
-        time_t ptr = time(0);
-        tm *lt = localtime(&ptr);
-        year = 1900 + lt->tm_year;
-        month = lt->tm_mon;
-        day = lt->tm_mday;
-        hour = lt->tm_hour;
-        minute = lt->tm_min;
-        second = lt->tm_sec;
+        __init();
     }
     void __init()
     {
@@ -33,7 +26,36 @@ public:
         minute = lt->tm_min;
         second = lt->tm_sec;
     }
-
+    void print() {
+        // Year/Month/Day Hour:Minute:Second
+        printf("%d/", year);
+        if(month<10) {
+            printf("0%d/", month);
+        } else {
+            printf("%d/", month);
+        }
+        if(day<10) {
+            printf("0%d ", day);
+        } else {
+            printf("%d ", day);
+        }
+        if(hour<10) {
+            printf("0%d:", hour);
+        } else {
+            printf("%d:", hour);
+        }
+        if(minute<10) {
+            printf("0%d:", minute);
+        } else {
+            printf("%d:", minute);
+        }
+        if(second<10) { 
+            printf("0%d", second);
+        } else {
+            printf("%d", second);
+        }
+        printf("\n");
+    }
 private:
     int year, month, day, hour, minute, second;
 };
