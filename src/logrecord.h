d956f3a7bc4b0e68fb088294a005ad9ce1402367
Ambitions0x39e <gwuchanga@gmail.com>
2023-12-14 14:34:08 +0800

    Uodate dates.h

---
+++ src/logrecord.h
@@ -0,0 +1,13 @@
+#include "date.h"
+#include "iostream"
+#include "deftypes.h"
+#include "filesystem"
+#include "system"
+using namespace std;
+/*
+TODO:
+- This header provides a function: 
+  recordLog(date actionDate, char * fileExecuted, bool error, char * errorArgcp[])
+  - actionDate is from "date.h", provides the time for the current action
+  - 
+*/
