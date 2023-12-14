a81fa820029495e492e5ed2facd5ab14325ae0dc
Ambitions0x39e <gwuchanga@gmail.com>
2023-12-14 21:07:51 +0800

    Update logrecord.h

--- src/logrecord.h
+++ src/logrecord.h
@@ -2,12 +2,11 @@
 #include "iostream"
 #include "deftypes.h"
 #include "filesystem"
-#include "system"
 using namespace std;
 /*
 TODO:
-- This header provides a function: 
-  recordLog(date actionDate, char * fileExecuted, bool error, char * errorArgcp[])
-  - actionDate is from "date.h", provides the time for the current action
-  - 
+recordLog(date actionDate, char * fileExecuted, bool error, char * errorArgcp[])
+actionDate is from "date.h", provides the time for the current action
 */
+/// @brief 
+void recordLog(date actionDate, char * fileExecuted, bool error, char * errorArgcp[]);
