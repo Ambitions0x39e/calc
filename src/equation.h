f957f52e37c4eedc5735775772651285fce0b43b
Ambitions0x39e <gwuchanga@gmail.com>
2023-12-23 21:01:31 +0800

    Create equation.h

---
+++ src/equation.h
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cstring>
+#include <string>
+/// @brief check if the string is available for string actions
+/// @param prompt 
+/// @return true / false
+
+bool equation_available(std::string prompt) {   
+    
+}
